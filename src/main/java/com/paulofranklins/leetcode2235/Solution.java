e9265d58fd6511ec9602fd459696f7ef1a8db8e1
paulofranklins <paulofranklinc@gmail.com>
2023-12-15 14:05:35 -0800

    Completed Problem 2235

---
+++ src/main/java/com/paulofranklins/leetcode2235/Solution.java
@@ -0,0 +1,12 @@
+package com.paulofranklins.leetcode2235;
+
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        var x = new Solution().sum(2,22);
+        System.out.println(x);
+    }
+}
