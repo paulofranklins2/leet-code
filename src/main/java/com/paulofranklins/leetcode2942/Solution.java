7adeebfd0d12d2368295a7a9760996d4b6b61e6e
paulofranklins <paulofranklinc@gmail.com>
2023-12-15 14:05:41 -0800

    Completed Problem 2942

---
+++ src/main/java/com/paulofranklins/leetcode2942/Solution.java
@@ -0,0 +1,20 @@
+package com.paulofranklins.leetcode2942;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        var result = new ArrayList<Integer>();
+        for (int i = 0; i < words.length; i++)
+            if (words[i].contains(Character.toString(x))) {
+                result.add(i);
+            }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        var x = new Solution().findWordsContaining(new String[] {"leet","no","code", "dev"}, 'e');
+        System.out.println(x);
+    }
+}
