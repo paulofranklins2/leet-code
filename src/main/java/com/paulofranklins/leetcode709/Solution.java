58b48e8d6c7ef39835845edfd3299def4c140ef7
paulofranklins <paulofranklinc@gmail.com>
2023-12-17 23:28:12 -0800

    Completed: 709

---
+++ src/main/java/com/paulofranklins/leetcode709/Solution.java
@@ -0,0 +1,15 @@
+package com.paulofranklins.leetcode709;
+
+class Solution {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+
+    }
+
+    public static void main(String[] args) {
+        var s = new Solution().toLowerCase("LoveLy");
+        System.out.println(s);
+
+    }
+}
+
