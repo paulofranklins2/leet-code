7d08d5b7dcb18ac7a2d51e99155125b2686c005f
paulofranklins <paulofranklinc@gmail.com>
2023-12-20 13:18:39 -0800

    Changed comment section

--- src/main/java/com/paulofranklins/leetcode557/Solution.java
+++ src/main/java/com/paulofranklins/leetcode557/Solution.java
@@ -7,37 +7,39 @@ class Solution {
 
         /*Option 1, re-inventing the wheel lol*/
 
-//        var temp = new StringBuilder();
-//        var list = new ArrayList<String>();
-//        var rs = new ArrayList<String>();
-//        for (int i = 0; i < s.length(); i++) {
-//            if (s.charAt(i) != ' ') {
-//                temp.append(s.charAt(i));
-//            } else {
-//                list.add(temp.toString());
-//                temp.delete(0, temp.length());
-//            }
-//        }
-//        list.add(temp.toString());
-//        temp.delete(0, temp.length());
-//
-//        for (int i = 0; i < list.size(); i++) {
-//            for (int j = list.get(i).length() - 1; j >= 0; j--) {
-//                temp.append(list.get(i).charAt(j));
-//            }
-//            if (i < list.size() - 1) {
-//                var z = temp.append(" ");
-//                rs.add(z.toString());
-//                temp.delete(0, temp.length());
-//            } else {
-//                rs.add(temp.toString());
-//                temp.delete(0, temp.length());
-//            }
-//        }
-//        for (String n : rs) {
-//            temp.append(n);
-//        }
-//        return temp.toString();
+        /*
+        var temp = new StringBuilder();
+        var list = new ArrayList<String>();
+        var rs = new ArrayList<String>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ') {
+                temp.append(s.charAt(i));
+            } else {
+                list.add(temp.toString());
+                temp.delete(0, temp.length());
+            }
+        }
+        list.add(temp.toString());
+        temp.delete(0, temp.length());
+
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = list.get(i).length() - 1; j >= 0; j--) {
+                temp.append(list.get(i).charAt(j));
+            }
+            if (i < list.size() - 1) {
+                var z = temp.append(" ");
+                rs.add(z.toString());
+                temp.delete(0, temp.length());
+            } else {
+                rs.add(temp.toString());
+                temp.delete(0, temp.length());
+            }
+        }
+        for (String n : rs) {
+            temp.append(n);
+        }
+        return temp.toString();
+        */
 
 
         /*Option 2 using StringBuilder reverse.*/
